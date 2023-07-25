7f63ce3338dd4985e9d85acaa7216795a0fb201b
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-07-25 18:28:15 +0100

    Boilerplate for input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{self, Read};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = io::stdout().into_raw_mode().unwrap();
+
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
+                }
+                Key::Ctrl('q') => {
+                    break;
+                }
+                _ => println!("{:?}\r", key),
+            },
+            Err(e) => die(e),
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
 }
