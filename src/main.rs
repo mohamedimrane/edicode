9ff22f5090ef8b8423b829a21d439639f38de326
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-07-25 21:03:32 +0100

    Display welcome message at enter

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Write};
 use termion::{event::Key, input::TermRead, raw::IntoRawMode};
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 fn main() {
     let _stdout = io::stdout().into_raw_mode().unwrap();
     let mut should_quit = false;
@@ -14,7 +16,7 @@ fn main() {
         if should_quit {
             break;
         } else {
-            draw_rows(terminal_size.1);
+            draw_rows(terminal_size);
             println!("{}", termion::cursor::Goto(1, 1));
         }
 
@@ -62,11 +64,27 @@ fn read_key() -> Result<Key, io::Error> {
     }
 }
 
-fn draw_rows(terminal_height: u16) {
-    for _ in 0..terminal_height - 1 {
+fn draw_rows(terminal_size: (u16, u16)) {
+    for row in 0..terminal_size.1 - 1 {
         print!("{}", termion::clear::CurrentLine);
-        println!("~\r");
+
+        if row == terminal_size.1 / 3 {
+            draw_welcome_message(terminal_size);
+        } else {
+            println!("~\r");
+        }
     }
+}
+
+fn draw_welcome_message(terminal_size: (u16, u16)) {
+    let mut welcome_message = format!("Edicode -- version {}", VERSION);
+    let width = terminal_size.0 as usize;
+    let len = welcome_message.len();
+    let padding = width.saturating_sub(len) / 2;
+    let spaces = " ".repeat(padding.saturating_sub(1));
+    welcome_message = format!("~{}{}", spaces, welcome_message);
+    welcome_message.truncate(width);
+    println!("{}\r", welcome_message);
 }
 
 fn die(e: io::Error) {
