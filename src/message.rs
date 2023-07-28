1615216febd5504980125a191d2bfefc427849fd
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-07-28 14:53:53 +0100

    Implement std::fmt::Display trait for message::Message

--- src/message.rs
+++ src/message.rs
@@ -13,3 +13,27 @@ impl<'a> Message<'a> {
         Self { kind, message }
     }
 }
+
+impl<'a> std::fmt::Display for Message<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use termion::color::{Fg, Reset, Rgb};
+        use MessageType::*;
+
+        // let color: dyn color::Color = match self.kind {
+        //     Normal => Reset,
+        //     Error => Rgb(255, 0, 0),
+        // };
+        // write!(
+        //     f,
+        //     "{}{}{}",
+        //     Fg(Red),
+        //     self.message,
+        //     Fg(Reset)
+        // )
+
+        match self.kind {
+            Normal => write!(f, "{}{}", Fg(Reset), self.message),
+            Error => write!(f, "{}{}{}", Fg(Rgb(255, 0, 0)), self.message, Fg(Reset)),
+        }
+    }
+}
